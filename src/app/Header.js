5f61f74e7c01f19fa74c55b95982eee982a1f904
Alexander Kohonovsky <saniaky@gmail.com>
2019-09-05 13:53:20 -0500

    Add mobile menu.

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {Link} from "react-router-dom";
+
+const HeaderWrapper = styled.div`
+`;
 
 const Header = styled.header`
     display: flex;
@@ -8,6 +11,10 @@ const Header = styled.header`
     border-bottom: 1px solid #e9eced;
     color: #676d73;
     height: 60px;
+    
+    @media (max-width: 768px) {
+        display: none
+    }
 `;
 
 const Logo = styled.div`
@@ -19,7 +26,7 @@ const Logo = styled.div`
 const Links = styled.div`
     display: flex;
     
-    > a {
+    a {
         color: #676d73;
         padding: 0 16px;
         font-size: 14px;
@@ -27,27 +34,95 @@ const Links = styled.div`
         align-items: center;
     }
     
-    > a:hover {
+    a:hover {
         box-shadow: inset 0 -2px 0 #d3d4d5;
         color: #2f3033;
     }
 `;
 
+const MobileHeader = styled.header`
+    display: none;
+    justify-content: space-between;
+    border-bottom: 1px solid #e9eced;
+    color: #676d73;
+    height: 60px;
+    
+    @media (max-width: 768px) {
+        display: flex;
+    }
+`;
+
+const MenuHead = styled.div`
+    padding: 0 16px;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+`;
+
+const MenuList = styled.div`
+    position: fixed;
+    transform: ${props => props.open ? "translateY(60px)" : "translateY(-100%)"};
+    transition: transform .2s ease-in;
+    background-color: #fff;
+    width: 100%;
+    height: 100%;
+    padding: 10px 10px;
+    font-size: 20px;
+    font-weight: 800;
+    color: black;
+    
+    ul {
+        list-style-type: none;
+    }
+    
+    li {
+        padding-bottom: 20px;
+    }
+`;
+
 export default () => {
 
-    return (
-        <Header>
-            <Logo>
-                <Link to="/">
-                    <img height="17px" width="197px" src={require("./logo.png")} alt="Logo"/>
-                </Link>
-            </Logo>
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-            <Links>
-                <Link to="/signup">Join as a photographer</Link>
-                <Link to="/signup">Sign up</Link>
-                <Link to="/login">Login</Link>
-            </Links>
-        </Header>
+    function toggleMobileMenu() {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    }
+
+    return (
+        <HeaderWrapper>
+            <Header>
+                <Logo>
+                    <Link to="/">
+                        <img height="17px" width="197px" src={require("./logo.png")} alt="Logo"/>
+                    </Link>
+                </Logo>
+
+                <Links>
+                    <Link to="/signup">Join as a photographer</Link>
+                    <Link to="/signup">Sign up</Link>
+                    <Link to="/login">Login</Link>
+                </Links>
+            </Header>
+
+            <MobileHeader>
+                <MenuHead>
+                    <button onClick={toggleMobileMenu}>
+                        <img height="17px" width="197px" src={require("./logo.png")} alt="Logo"/>
+                        <img src={require(isMobileMenuOpen ? './up-arrow.svg' : './down-arrow.svg')}
+                             alt=""
+                             style={{marginLeft: '10px', height: '13px', width: '13px'}}
+                        />
+                    </button>
+                </MenuHead>
+                <MenuList open={isMobileMenuOpen}>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="join">Join as a photographer</Link></li>
+                        <li><Link to="signup">Sign up</Link></li>
+                        <li><Link to="login">Log in</Link></li>
+                    </ul>
+                </MenuList>
+            </MobileHeader>
+        </HeaderWrapper>
     )
 }
