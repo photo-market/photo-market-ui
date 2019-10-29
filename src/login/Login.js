7d524a641ee1af003b7dfc78741a1a8b43491e6b
Alexander Kohonovsky <saniaky@gmail.com>
2019-10-29 12:56:48 -0500

    Update node version in dockerfile

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
+
+const Main = styled.div`
+    display: flex;
+    vertical-align: center;
+`;
 
 export default () => {
 
     return (
-        <div>
-            Login
-        </div>
+        <Main>
+            <form>
+                <div>
+                    Login <input type="text" name="username"/>
+                </div>
+                <div>
+                    Password <input type="password" name="password"/>
+                </div>
+            </form>
+            <Link to="/signup">Don't have an account? Register here.</Link>
+        </Main>
     );
 }
