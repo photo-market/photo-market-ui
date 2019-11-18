b38959214e96ddb86474c8e5de8f070a6897ffb6
Alexander Kohonovsky <saniaky@gmail.com>
2019-11-18 16:58:11 -0600

    Fix build - update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "Loggin in to dockerhub..."
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ACCOUNT" --password-stdin
 
 # Push latest tag
+echo "Pushing image with the lastest tag..."
 docker push ${IMAGE_NAME}
 
 # Push specific tag
+echo "Pushing specific image tag..."
 NEW_IMAGE_NAME=${IMAGE_NAME}:${TRAVIS_BUILD_NUMBER}
 docker tag ${IMAGE_NAME} ${NEW_IMAGE_NAME}
 docker push ${NEW_IMAGE_NAME}
-
-# Run
-# docker run --rm -p 9000:80 saniaky/photo-market-ui:latest
