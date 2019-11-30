a581318b04af4b9bdb06f5c5906a70b428714296
Alexander Kohonovsky <saniaky@gmail.com>
2019-11-30 13:55:18 -0600

    Update .env file, remove cognito trash.

--- src/common/consts/userAuth.js
+++ src/common/consts/userAuth.js
@@ -8,10 +8,5 @@ export const DEFAULT_USER = Object.freeze({
     gender: '',
 });
 
-export const COGNITO_POOL_DATA = Object.freeze({
-    UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-    ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID
-});
-
 export const USER_PROFILE_KEY = "user_profile";
 
--- src/config.js
+++ src/config.js
@@ -3,12 +3,6 @@ export default {
         REGION: "",
         BUCKET: ""
     },
-    cognito: {
-        REGION: "",
-        USER_POOL_ID: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-        APP_CLIENT_ID: process.env.REACT_APP_COGNITO_CLIENT_ID,
-        IDENTITY_POOL_ID: ""
-    },
     social: {
         GOOGLE: "",
         FB: ""
